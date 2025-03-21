42d68aea911be9192e122d0f51771546424579e1
Matej Bačo <matejbaco2000@gmail.com>
2025-03-21 10:34:28 +0000

    Add Nuxt OneLink

---
+++ nuxt/onelink/components/Homepage.vue
@@ -0,0 +1,102 @@
+<template>
+  <main
+    class="p-4 bg-white dark:bg-neutral-900 h-full w-full space-y-8 pt-12 max-w-sm mx-auto"
+  >
+    <div class="text-center">
+      <div
+        v-if="acc.photo"
+        class="mb-6 h-20 w-20 rounded-full overflow-hidden ring ring-neutral-200 dark:ring-neutral-800 mx-auto"
+      >
+        <img :src="acc.photo" alt="name" class="h-full w-full object-cover" />
+      </div>
+      <h1
+        v-if="acc.name"
+        class="text-2xl font-bold mt-4 text-neutral-800 dark:text-neutral-100"
+      >
+        {{ acc.name }}
+      </h1>
+      <p v-if="acc.d" class="text-sm mt-2 text-neutral-600">
+        {{ acc.description }}
+      </p>
+    </div>
+    <div
+      class="flex items-center justify-center flex-wrap text-black dark:text-neutral-300"
+    >
+      <span v-if="acc.facebook" class="p-1">
+        <a :href="acc.facebook" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:facebook-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.twitter" class="p-1">
+        <a :href="acc.twitter" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:twitter-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.instagram" class="p-1">
+        <a :href="acc.instagram" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:instagram-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.email" class="p-1">
+        <a :href="acc.email" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:envelope-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.telegram" class="p-1">
+        <a :href="acc.telegram" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:telegram-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.phone" class="p-1">
+        <a
+          :href="`https://wa.me/${acc.w}`"
+          target="_blank"
+          rel="noopener | noreferrer"
+        >
+          <Icon name="ph:whatsapp-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.youtube" class="p-1">
+        <a :href="acc.youtube" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:youtube-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.email" class="p-1">
+        <a
+          :href="`mailto:${acc.email}`"
+          target="_blank"
+          rel="noopener | noreferrer"
+        >
+          <Icon name="ph:envelope-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.github" class="p-1">
+        <a :href="acc.github" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:github-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+      <span v-if="acc.linkedin" class="p-1">
+        <a :href="acc.linkedin" target="_blank" rel="noopener | noreferrer">
+          <Icon name="ph:linkedin-logo-duotone" class="h-6 w-6" />
+        </a>
+      </span>
+    </div>
+    <ul class="space-y-2">
+      <ExternalLink
+        v-for="(link, id) in acc.links"
+        :label="link.title"
+        :icon="link.icon"
+        :url="link.url"
+        :key="id"
+      />
+    </ul>
+  </main>
+</template>
+<script setup>
+const props = defineProps({
+  acc: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
---
+++ nuxt/onelink/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <homepage :acc="account" />
+  </div>
+</template>
+<script setup>
+const account = {
+  name: "John Doe",
+  description: "Im John Doe, the very first user of Appwrite.",
+  photo: "https://cloud.appwrite.io/v1/avatars/initials?name=John%20Doe",
+  facebook: "https://www.facebook.com/appwrite_john_doe",
+  twitter: "https://twitter.com/appwrite_john_doe",
+  instagram: "https://www.instagram.com/appwrite_john_doe",
+  email: "john@appwrite.io",
+  github: "https://github.com/appwrite_john_doe",
+  telegram: "https://t.me/appwrite_john_doe",
+  phone: "+4930901820",
+  youtube: "https://youtube.com/@appwrite_john_doe",
+  linkedin: "https://linkedin.com/appwrite_john_doe",
+  // For link icons, see: https://nuxt.com/modules/icon
+  links: [
+    {
+      title: "My Website",
+      icon: "ph:facebook-logo-duotone",
+      url: "https://appwrite.io",
+    },
+    {
+      title: "Amazon wishlist",
+      icon: "ant-design:amazon-outlined",
+      url: "https://amazon.in",
+    },
+    {
+      title: "React JS course",
+      icon: "grommet-icons:reactjs",
+      url: "https://reactjs.org/",
+    },
+    {
+      title: "Donate for good cause",
+      icon: "iconoir:donate",
+      url: "https://who.int",
+    },
+  ],
+};
+</script>
