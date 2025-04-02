f107cfa1c097db220cbcd7becb988dca88c15da9
Matej Bačo <matejbaco2000@gmail.com>
2025-04-02 16:53:58 +0200

    Fix web imports of lynx js web components

--- lynx/gallery/web/src/App.tsx
+++ lynx/gallery/web/src/App.tsx
@@ -1,12 +1,13 @@
-import "./App.css";
-import "@lynx-js/web-core/index.css";
-import "@lynx-js/web-elements/index.css";
-import "@lynx-js/web-core";
+import './App.css';
+import '@lynx-js/web-core/index.css';
+import '@lynx-js/web-elements/index.css';
+import '@lynx-js/web-core';
+import '@lynx-js/web-elements/all';
 
 const App = () => {
   return (
     <lynx-view
-      style={{ height: "100vh", width: "100vw" }}
+      style={{ height: '100vh', width: '100vw' }}
       url="/main.web.bundle"
     ></lynx-view>
   );
